32aaaf61f0dc50aff8c1e9273be5714c11734bfa
Tenura Pasandul <57010235+TenuraPasandul@users.noreply.github.com>
2024-07-10 00:12:30 +0530

    Add files via upload

--- admin/messages.php
+++ admin/messages.php
@@ -14,15 +14,15 @@ if(isset($_POST['delete'])){
    $delete_id = $_POST['delete_id'];
    $delete_id = filter_var($delete_id, FILTER_SANITIZE_STRING);
 
-   $verify_delete = $conn->prepare("SELECT * FROM `admins` WHERE id = ?");
+   $verify_delete = $conn->prepare("SELECT * FROM `messages` WHERE id = ?");
    $verify_delete->execute([$delete_id]);
 
    if($verify_delete->rowCount() > 0){
-      $delete_admin = $conn->prepare("DELETE FROM `admins` WHERE id = ?");
-      $delete_admin->execute([$delete_id]);
-      $success_msg[] = 'Admin deleted!';
+      $delete_bookings = $conn->prepare("DELETE FROM `messages` WHERE id = ?");
+      $delete_bookings->execute([$delete_id]);
+      $success_msg[] = 'Message deleted!';
    }else{
-      $warning_msg[] = 'Admin deleted already!';
+      $warning_msg[] = 'Message deleted already!';
    }
 
 }
@@ -35,7 +35,7 @@ if(isset($_POST['delete'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Admins</title>
+   <title>Messages</title>
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
@@ -48,10 +48,10 @@ if(isset($_POST['delete'])){
 
 <section class="grid">
 
-   <h1 class="heading">admins</h1>
+   <h1 class="heading">messages</h1>
 
    <form action="" method="POST" class="search-form">
-      <input type="text" name="search_box" placeholder="search admins..." maxlength="100" required>
+      <input type="text" name="search_box" placeholder="search messages..." maxlength="100" required>
       <button type="submit" class="fas fa-search" name="search_btn"></button>
    </form>
 
@@ -61,43 +61,32 @@ if(isset($_POST['delete'])){
       if(isset($_POST['search_box']) OR isset($_POST['search_btn'])){
          $search_box = $_POST['search_box'];
          $search_box = filter_var($search_box, FILTER_SANITIZE_STRING);
-         $select_admins = $conn->prepare("SELECT * FROM `admins` WHERE name LIKE '%{$search_box}%'");
-         $select_admins->execute();
+         $select_messages = $conn->prepare("SELECT * FROM `messages` WHERE name LIKE '%{$search_box}%' OR number LIKE '%{$search_box}%' OR email LIKE '%{$search_box}%'");
+         $select_messages->execute();
       }else{
-         $select_admins = $conn->prepare("SELECT * FROM `admins`");
-         $select_admins->execute();
+         $select_messages = $conn->prepare("SELECT * FROM `messages`");
+         $select_messages->execute();
       }
-      if($select_admins->rowCount() > 0){
-         while($fetch_admins = $select_admins->fetch(PDO::FETCH_ASSOC)){
+      if($select_messages->rowCount() > 0){
+         while($fetch_messages = $select_messages->fetch(PDO::FETCH_ASSOC)){
    ?>
-   <?php if( $fetch_admins['id'] == $admin_id){ ?>
-   <div class="box" style="order: -1;">
-      <p>name : <span><?= $fetch_admins['name']; ?></p>
-      <a href="update.php" class="option-btn">update account</a>
-      <a href="register.php" class="btn">register new</a>
-   </div>
-   <?php }else{?>
    <div class="box">
-      <p>name : <span><?= $fetch_admins['name']; ?></p>
+      <p>name : <span><?= $fetch_messages['name']; ?></span></p>
+      <p>email : <a href="mailto:<?= $fetch_messages['email']; ?>"><?= $fetch_messages['email']; ?></a></p>
+      <p>number : <a href="tel:<?= $fetch_messages['number']; ?>"><?= $fetch_messages['number']; ?></a></p>
+      <p>message : <span><?= $fetch_messages['message']; ?></span></p>
       <form action="" method="POST">
-         <input type="hidden" name="delete_id" value="<?= $fetch_admins['id']; ?>">
-         <input type="submit" value="delete admin" onclick="return confirm('delete this admin?');" name="delete" class="delete-btn">
+         <input type="hidden" name="delete_id" value="<?= $fetch_messages['id']; ?>">
+         <input type="submit" value="delete message" onclick="return confirm('delete this message?');" name="delete" class="delete-btn">
       </form>
    </div>
-   <?php } ?>
    <?php
       }
    }elseif(isset($_POST['search_box']) OR isset($_POST['search_btn'])){
-      echo '<p class="empty">no results found!</p>';
+      echo '<p class="empty">results not found!</p>';
    }else{
-   ?>
-      <p class="empty">no admins added yet!</p>
-      <div class="box" style="text-align: center;">
-      <p>create a new admin</p>
-      <a href="register.php" class="btn">register now</a>
-   </div>
-   <?php
-      }
+      echo '<p class="empty">you have no messages!</p>';
+   }
    ?>
 
    </div>
