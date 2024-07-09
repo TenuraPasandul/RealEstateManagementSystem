7d7425a2b7690d111ba851d25dfcbb845f53bf62
Tenura Pasandul <57010235+TenuraPasandul@users.noreply.github.com>
2024-07-09 23:51:26 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:+94777092346"><i class="fas fa-phone"></i><span>+94 777 092 346</span></a>
+         <a href="tel:+94112345654"><i class="fas fa-phone"></i><span>+94 112 345 654</span></a>
+         <a href="msdgoldenlandmark@gmail.com"><i class="fas fa-envelope"></i><span>msdgoldenlandmark@gmail.com</span></a>
+         <a href="https://goo.gl/maps/8QUZ65emvmVSvhtw6" target="_blank"><i class="fas fa-map-marker-alt"></i><span> Uwa Wellassa University, Badulla</span></a>
+      </div>
+
+      <div class="box">
+      <a href="home.php"><span><img src="images/logo.png"></span></a>
+      <a href="home.php"><span><p>MSD Golden LandMark</p></span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>MSD Golden LandMark</span> | all rights reserved!</div>
+
+</footer>
